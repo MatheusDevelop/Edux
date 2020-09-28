5d28d2e66895a429c161e81c3adc1648b9acfbd2
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-28 14:25:48 -0300

    Corrigindo authorize do Turma controller

--- Edux_API/Controllers/TurmaController.cs
+++ Edux_API/Controllers/TurmaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Edux_API.Domains;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,7 @@ namespace Edux_API.Controllers
 
 
     [ApiController]
+    [Authorize(Roles = "Professor,Instituicao,Admin")]
     public class TurmaController : GenericController<Turma>
     {
     }
