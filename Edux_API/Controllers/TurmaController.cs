60a6491947252b1c227e8cf3f963fc2730648134
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-28 14:22:13 -0300

    projeto finalizado

--- Edux_API/Controllers/TurmaController.cs
+++ Edux_API/Controllers/TurmaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Edux_API.Domains;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +10,7 @@ namespace Edux_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TurmaController : ControllerBase
+    public class TurmaController : GenericController<Turma>
     {
     }
 }
