ba579ffe5a2e065c534a9ba15b142f5697bb8d26
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-29 15:57:31 -0300

    Subindo arquivos

--- Edux_API/Controllers/Generic/GenericController.cs
+++ Edux_API/Controllers/Generic/GenericController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Edux_API.Interfaces;
 using Edux_API.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -14,14 +13,12 @@ namespace Edux_API.Controllers
     [ApiController]
     public class GenericController<T> : ControllerBase where T: class
     {
+
+        BaseGeneric<T> repo = new BaseGeneric<T>();
        
-        private readonly IBase<T> repo;
 
 
-        public GenericController(IBase<T> _repo)
-        {
-            repo = _repo;
-        }
+        
         
         /// <summary>
         /// Retorna uma lista mostrando todos os objetos da requisiçao
--- Edux_API/Interfaces/IBase.cs
+++ Edux_API/Interfaces/IBase.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Edux_API.Interfaces
 {
-    interface IBase<T> where T: class
+    public interface IBase<T> where T: class
     {
         T BuscarPorId(Guid id);
         List<T> BuscarTodos();
