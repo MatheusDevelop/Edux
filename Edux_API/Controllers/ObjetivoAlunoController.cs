a89ee59785b9ff35453bee6e27d506021317950c
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-30 14:55:32 -0300

    ...

--- Edux_API/Controllers/ObjetivoAlunoController.cs
+++ Edux_API/Controllers/ObjetivoAlunoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Edux_API.Domains;
 using Edux_API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,7 @@ namespace Edux_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Professor,Instituicao,Admin")]
     public class ObjetivoAlunoController : GenericController<ObjetivoAluno>
     {
         public ObjetivoAlunoController(IBase<ObjetivoAluno> repos):base(repos)
